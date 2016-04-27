66028b6941e3e7e8b81fa518ee5a07bc9b27fbe0
Ted Pattison <ted@tedpattison.net>
2016-04-27 12:34:18 -0400

    Initial Upload

---
+++ RoleCall.ps1
@@ -0,0 +1,20 @@
+﻿cls
+Write-Host 
+
+
+$TenantDomainName = "PBIClassroom123"
+Write-Host "Classroom Domain: $TenantDomainName"
+Write-Host 
+
+$CurrentDirectory = Get-Location 
+$StudentsFilePath = ("{0}\Students.csv" -f $CurrentDirectory.Path)
+$Students = Import-csv -path $StudentsFilePath
+foreach($Student in $Students) { 
+  $StudentUPN = $Student.FirstName + "." + $Student.LastName + "@" + $TenantDomainName + ".onMicrosoft.com"
+  $StudentDisplayName = $Student.FirstName + " " + $Student.LastName
+  Write-Host ($StudentUPN)
+  
+}
+
+
+Write-Host 
